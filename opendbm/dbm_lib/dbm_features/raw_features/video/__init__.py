12445e3c20876b8a6ade98a1b4cdcdc57c675279
jordi.hasianta <jordi.hasianta@gmail.com>
2022-09-15 20:52:32 +0700

    refactoring import with init

--- opendbm/dbm_lib/dbm_features/raw_features/video/__init__.py
+++ opendbm/dbm_lib/dbm_features/raw_features/video/__init__.py
@@ -4,11 +4,10 @@ project_name: DBM
 created: 2020-20-07
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
-import os
-
-DBMLIB_PATH = os.path.dirname(__file__)
-DBMLIB_FACE_CONFIG = os.path.abspath(os.path.join(DBMLIB_PATH, '../../../../resources/services/face_util.yml'))
+from .face_asymmetry import run_face_asymmetry
+from .face_au import run_face_au
+from .face_config.face_config_reader import ConfigFaceReader
+from .face_emotion_expressivity import run_face_expressivity
+from .face_landmark import run_face_landmark
