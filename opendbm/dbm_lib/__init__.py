08e9e8691c16ce7b9b05918013e33170fd3a73e9
jordi.hasianta <jordi.hasianta@gmail.com>
2022-09-15 21:30:43 +0700

    import refactoring with init

--- opendbm/dbm_lib/__init__.py
+++ opendbm/dbm_lib/__init__.py
@@ -4,13 +4,16 @@ project_name: DBM
 created: 2020-20-07
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
-import os
-
-DBMLIB_PATH = os.path.dirname(__file__)
-DBMLIB_SERVICE_CONFIG = os.path.abspath(os.path.join(DBMLIB_PATH, '../resources/services/services.yml'))
-DBMLIB_FEATURE_CONFIG = os.path.abspath(os.path.join(DBMLIB_PATH, '../resources/features/raw_feature.yml'))
-DBMLIB_DERIVE_FEATURE_CONFIG = os.path.abspath(os.path.join(DBMLIB_PATH, '../resources/features/derived_feature.yml'))
+from .config import config_derive_feature, config_raw_feature, config_reader
+from .dbm_features.raw_features.audio.formant_freq import run_formant
+from .dbm_features.raw_features.audio.intensity import run_intensity
+from .dbm_features.raw_features.audio.mfcc import run_mfcc
+from .dbm_features.raw_features.audio.pause_segment import run_pause_segment
+from .dbm_features.raw_features.movement.facial_tremor import fac_tremor_process
+from .dbm_features.raw_features.movement.head_motion import run_head_movement
+from .dbm_features.raw_features.nlp.speech_features import run_speech_feature
+from .dbm_features.raw_features.video import ConfigFaceReader
+from .dbm_features.raw_features.video.face_asymmetry import run_face_asymmetry
+from .dbm_features.raw_features.video.face_landmark import run_face_landmark
