006cb0eb0a19558cebd9549952f8feaaa7b19e6c
andre.paredes <andre.paredes@aicure.com>
2023-01-09 13:49:51 -0500

    cleaned notes in sample script VA section

--- sample_data/sample_script.py
+++ sample_data/sample_script.py
@@ -9,8 +9,8 @@ import siuba.dply.verbs
 import siuba.dply.vector
 
 #from opendbm import FacialActivity  #needed with docker?
-from opendbm import Movement as mv
-from opendbm import VerbalAcoustics as va #needed with docker? 
+# from opendbm import Movement as mv #needed with docker? 
+from opendbm import VerbalAcoustics as va 
 from opendbm import Speech as sp
 
 
@@ -26,10 +26,10 @@ path_file = list_mp4[0]
 ### VERBAL ACOUSTICS -----
 
 #fit the model
-model = va()
-model.fit(path_file)
+model_va = va()
+model_va.fit(path_file)
 #model_va = va().fit(pathfile) # one-line alternative
-var_intensity = model.get_audio_intensity()
+var_intensity = model_va.get_audio_intensity()
 
 # get audio intensity
 df_va_intensity = var_intensity.to_dataframe()
